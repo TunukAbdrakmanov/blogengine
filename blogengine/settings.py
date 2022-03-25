27db857bc087de93e95bb0adc167777eda4ac972
Tunuk Abdrakmanov <tunuk.abdrakmanov.pls@gmail.com>
2022-03-25 21:47:44 +0600

    part3

--- blogengine/settings.py
+++ blogengine/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'blogengine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project3',
+        'USER': 'tunuk',
+        'PASSWORD': '1',
     }
 }
 
